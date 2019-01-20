1768abe764e8d729de0b1dd0aa4a6b34447ac1a7
victoryeo <victoryeo1974@gmail.com>
2019-01-20 11:12:48 +0800

    add balance

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import logo from './logo-tiny.png';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo-tiny.png'
+import './App.css'
 import {
   BrowserRouter,
   Route,
@@ -8,12 +8,13 @@ import {
   NavLink,
   HashRouter,
   Redirect
-} from "react-router-dom";
-import Transactions from "./transactions";
-import GetBlocks from "./getblocks";
-import Mining from "./mining";
-import PropTypes from "prop-types";
-import { withRouter } from 'react-router';
+} from "react-router-dom"
+import Transactions from "./transactions"
+import GetBlocks from "./getblocks"
+import GetBalance from "./getbalance"
+import Mining from "./mining"
+import PropTypes from "prop-types"
+import { withRouter } from 'react-router'
 
 class App extends Component {
   constructor(props) {
@@ -56,18 +57,20 @@ class App extends Component {
         <ul className="header">
           <li><NavLink to="/transactions">Transactions</NavLink></li>
           <li><NavLink to="/getblocks">GetBlocks</NavLink></li>
+          <li><NavLink to="/getbalance">GetBalance</NavLink></li>
           <li><NavLink to="/mining">Mining</NavLink></li>
         </ul>
           <div className="content">
             <Route path="/transactions" component={Transactions}/>
             <Route path="/getblocks" component={GetBlocks}/>
+            <Route path="/getbalance" component={GetBalance}/>
             <Route path="/mining" component={Mining}/>
           </div>
         </body>
       </div>
       </BrowserRouter>
-    );
+    )
   }
 }
 
-export default (App);
+export default (App)
